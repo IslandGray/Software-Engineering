e687f7894757fbde9330522d67d756877d1fbc9a
Vilod <wcp626954177@vip.qq.com>
2016-11-08 23:20:16 +0800

    Finish:developer index

---
+++ src/Dev/DevIndex.java
@@ -0,0 +1,20 @@
+package Dev;
+
+public class DevIndex {
+	private String inputAccount;
+	
+	public String login(){
+		return "SUCCESS";
+	}
+
+	public String getInputAccount() {
+		return inputAccount;
+	}
+
+	public void setInputAccount(String inputAccount) {
+		this.inputAccount = inputAccount;
+	}
+	
+	
+	
+}
